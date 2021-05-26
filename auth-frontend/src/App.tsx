88eec210b1b717874a74dedd9a6767db05fdcec6
Murilo Lopes <mloopes11@gmail.com>
2021-05-26 07:46:09 -0300

    global styles

--- auth-frontend/src/App.tsx
+++ auth-frontend/src/App.tsx
@@ -1,8 +1,11 @@
 import { Login } from "./pages/Login";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <Login />
+      <GlobalStyle />
     </div>
   );
 }
