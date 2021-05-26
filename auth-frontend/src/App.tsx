46447a28957f336e9caeee1fb5e566dded2a4cbc
Murilo Lopes <mloopes11@gmail.com>
2021-05-26 07:36:58 -0300

    styled-components

--- auth-frontend/src/App.tsx
+++ auth-frontend/src/App.tsx
@@ -1,17 +1,8 @@
-import GoogleLogin from "react-google-login";
-
+import { Login } from "./pages/Login";
 export function App() {
-  const responseGoogle = (response: any) => {
-    console.log(response);
-  };
   return (
     <div className="App">
-      <GoogleLogin
-        clientId="604854120587-h09ufc01lrb6j9v0t8boghrp2u84gilv.apps.googleusercontent.com"
-        buttonText="Continuar com o Google"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-      />
+      <Login />
     </div>
   );
 }
--- auth-frontend/src/pages/Login/index.tsx
+++ auth-frontend/src/pages/Login/index.tsx
@@ -1,1 +1,19 @@
-export function Login() {}
+import GoogleLogin from "react-google-login";
+import { useEffect, useState } from "react";
+import { Container } from "./styles";
+
+export function Login() {
+  const responseGoogle = (response: any) => {
+    console.log(response);
+  };
+  return (
+    <Container>
+      <GoogleLogin
+        clientId="604854120587-h09ufc01lrb6j9v0t8boghrp2u84gilv.apps.googleusercontent.com"
+        buttonText="Continuar com o Google"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+      />
+    </Container>
+  );
+}
